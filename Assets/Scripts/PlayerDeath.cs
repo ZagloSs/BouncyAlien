88d84da96dab2e191a8dcb78b9d572c3f6abaec0
ZagloSs <carlosdiezmendez@gmail.com>
2024-01-30 09:20:17 +0100

    fixed punctuation added new best score notification

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
     [SerializeField] private GameObject explosion;
+    [SerializeField] private GameObject bsTxt;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,7 +14,12 @@ public class PlayerDeath : MonoBehaviour
         {
             if(PlayerPrefs.GetInt("bs", 0) < Score.instance.getScore())
             {
+                bsTxt.SetActive(true);
                 PlayerPrefs.SetInt("bs", Score.instance.getScore());
+            }
+            else
+            {
+                bsTxt.SetActive(false);
             }
 
             GameManager.instance.GameOver();
