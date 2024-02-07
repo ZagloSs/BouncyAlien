3a592fdc91ab4a7ccb2f9c2a6a822deccafeb71d
ZagloSs <carlosdiezmendez@gmail.com>
2024-02-07 09:34:31 +0100

    Compilation exceptions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,11 +23,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
         {
             jump();
         }
-
+#elif UNITY_ANDROID
         foreach (Touch toque in Input.touches)
         {
             if (toque.phase == TouchPhase.Began)
@@ -35,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
                 jump();
             }
         }
+#endif
     }
 
     private void jump()
